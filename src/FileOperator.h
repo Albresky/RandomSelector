2f48679bbb8f958c5ae75d0bf8d5d22a6179adeb
Albresky <78648318+Albresky@users.noreply.github.com>
2022-05-06 15:46:38 +0800

    remove old code

--- src/FileOperator.h
+++ src/FileOperator.h
@@ -4,11 +4,7 @@
 #include "commonHeaders.h"
 #include <QTextStream>
 
-bool isFolder(const QString& pathName);
 bool isFile(const QString& pathName);
-bool fileCopy(const QString _res, const QString _target);
-bool copyPic(QString resFile, const QString& targetDir,QString num);
-bool storeIni(const QStringList &fileNameList, QString& targetDir);
 bool createFile(const QString& fileName);
 
 #endif // FILEOPERATOR_H
